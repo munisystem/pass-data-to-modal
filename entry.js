43c26330b51967fbfd938157e6549f8d626ee68b
Yuichi Saito <munisystem@gmail.com>
2017-10-11 14:36:51 +0900

    Add button

--- entry.js
+++ entry.js
@@ -5,6 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
         el: '#app',
         data: {
             message: 'Hello vue.js with webpack',
+            num: null,
+            items: [
+                {
+                    num: 10
+                },
+                {
+                    num: 20
+                },
+                {
+                    num: 30
+                },
+            ],
         },
     });
 });
